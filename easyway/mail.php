bdf6622520bbcc060b84ccaafeb3cdb69a2aabdb
Kate Matiash <kateryna.matiash@gmail.com>
2019-11-03 21:44:35 +0200

    fix

--- easyway/mail.php
+++ easyway/mail.php
@@ -2,7 +2,7 @@
 //Script Foreach
 $c = true;
 $project_name = trim($_POST["project_name"]);
-$admin_email  = trim($_POST["admin_email"]);
+$admin_email  = 'rudakov.a@profmk.ru';
 $form_subject = trim($_POST["form_subject"]);
 foreach ( $_POST as $key => $value ) {
  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
